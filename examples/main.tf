7e47ed26cad07c020cc1ec200066cd9edb196f5c
Raiko Koosaar <koosaar@live.com>
2021-06-10 15:11:50 +0100

    Verification and  Readme update

--- examples/main.tf
+++ examples/main.tf
@@ -2,10 +2,13 @@ module "az-log-analytics" {
   source = "../modules/terraform-azurerm-log-analytics"
 
   az_la_ws_name     = "az-log-analytics-ws-name"
-  az_la_ws_rg_name  = module.az-resource-group.az-rg-name
-  az_la_ws_location = module.az-resource-group.az-rg-location
+  az_la_ws_rg_name  = "ResourceGroup-Name"
+  az_la_ws_location = "UK South"
   #az_la_ws_sku = "PerGB2018"
   az_la_ws_retention_in_days = 180
+
+  # use this if you are creating a resource group first using RG module
+  #depends_on = [module.az-resource-group]
 
   az_tags = {
     Environment   = "Development"
